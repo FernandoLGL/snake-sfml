3c9e6e22fa0f52f6b25f186d0ad45fa469ed00c9
Fernando Rosendo <franplt629@gmail.com>
2019-11-19 15:34:53 -0300

    Treating snake movement on key press done ( MAIN)

--- src/main.cpp
+++ src/main.cpp
@@ -25,15 +25,47 @@ int main() {
 
         // event pending ?
         while (window.pollEvent(event)) {
+
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (event.type == sf::Event::KeyPressed){
+
+                switch(event.key.code){
+                case sf::Keyboard::Key::A:
+                case sf::Keyboard::Key::Left:
+                    // You can't move left or right if you're already moving left
+                    if(snake.getDirection() == Direction::LEFT || snake.getDirection() == Direction::RIGHT)
+                        break;
+                    snake.moveLeft();
+                    break;
+                case sf::Keyboard::Key::Right:
+                case sf::Keyboard::Key::D:
+                    if(snake.getDirection() == Direction::RIGHT || snake.getDirection() == Direction::LEFT)
+                        break;
+                    snake.moveRight();
+                    break;
+
+                case sf::Keyboard::Key::Up:
+                case sf::Keyboard::Key::W:
+                    if(snake.getDirection() == Direction::UP || snake.getDirection() == Direction::DOWN)
+                        break;
+                    snake.moveUp();
+                    break;
+                case sf::Keyboard::Key::S:
+                case sf::Keyboard::Key::Down:
+                    if(snake.getDirection() == Direction::UP || snake.getDirection() == Direction::DOWN)
+                        break;
+                    snake.moveDown();
+                    break;
+                 default:
+                    break;
+                }
+            }
         }
 
-        // Drawing the whole snake
         snake.draw(window);
-
-        // Drawing food
-        window.draw(food);
+        snake.continueMoving();
 
         window.display();
     }
