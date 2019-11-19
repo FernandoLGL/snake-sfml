518a02da66def216e344fd2ed790742b0329684b
Fernando Rosendo <franplt629@gmail.com>
2019-11-19 14:21:21 -0300

    Added width, neight and block size to constants

--- include/Constants.h
+++ include/Constants.h
@@ -1,6 +1,11 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+constexpr int windowWidth = 800;
+constexpr int windowHeight = 600;
+
+constexpr float blockSize = 10.f;
+
 enum class Direction{
     UP,
     DOWN,
