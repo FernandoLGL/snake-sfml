0ea347c5943532c45c98d00e8f65dfb3f851e698
Fernando Rosendo <franplt629@gmail.com>
2019-11-20 09:08:19 -0300

    Food respawn without checking for respawning in the body

--- src/Food.cpp
+++ src/Food.cpp
@@ -1,8 +1,16 @@
 #include "Food.h"
+#include <cstdlib>
 
 Food::Food(const float &x, const float &y, const float &size, const sf::Color &color)
     : sf::RectangleShape(sf::Vector2f(size,size))
 {
     this->setPosition(x, y);
     this->setFillColor(color);
+}
+
+void Food::respawn(){
+    std::srand(std::time(NULL));
+    int randomX = (std::rand() % windowWidth) + 1;
+    int randomY = (std::rand() % windowHeight) + 1;
+    this->setPosition(randomX, randomY);
 }
